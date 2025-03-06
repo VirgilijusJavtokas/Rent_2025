ed7ec9a19a3d3c84c467ae8e0e4ed8b2990edb55
virgilijusjavtokas <virgilijus.javtokas@gmail.com>
2025-03-06 14:32:47 +0200

    Sudetos grupiu foto ir sutvarkytas Footer

--- mysite/rent/views.py
+++ mysite/rent/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 from .models import Group, Product, Status
 
 def index(request):
-    num_products = Product.objects.count()
-    num_status = Status.objects.count()
-    num_groups = Group.objects.count()
+    num_products = Product.objects.all().count()
+    num_status = Status.objects.all().count()
+    num_groups = Group.objects.all().count()
+
     context = {
         'num_products': num_products,
         'num_status': num_status,
         'num_groups': num_groups,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'base.html', context)
