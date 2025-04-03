63eb8498ec3181427b9e710c96b4686677d717ca
virgilijusjavtokas <virgilijus.javtokas@gmail.com>
2025-04-03 15:09:12 +0300

    Atnaujintas settings failas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_URL = '/static/'
 STATIC_ROOT = STATIC_ROOT
 STATICFILES_DIRS = STATICFILES_DIRS
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
